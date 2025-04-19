877da4cdd856555af0369fb663f2caa86d4fd651
Cmb <71194682+29cmb@users.noreply.github.com>
2025-04-19 09:06:49 -0400

    feat(client): ✨ Homepage

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,12 +1,19 @@
 import "@/styles/globals.css";
 import { Metadata } from "next";
 import type { AppProps } from "next/app";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Layout({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <main className={inter.className}>
+            <Component {...pageProps} />
+        </main>
+    );
 }
 
 export const metadata: Metadata = {
     title: "Commission Tracker",
-    description: "Track current commissions."
-}
+    description: "Track current commissions.",
+};
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,6 +1,11 @@
+import '@/styles/index.css'
 const HomePage = () => {
     return <>
-        
+        <h1>The easiest way to manage your commissions</h1> 
+        <div id="buttons">
+            <button>Login</button>
+            <button>Downloads</button>
+        </div>
     </>
 }
 
